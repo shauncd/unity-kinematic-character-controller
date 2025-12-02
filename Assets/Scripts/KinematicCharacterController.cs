8e32167757a72361c1f1f56706e8cf34b96379f3
shauncd <shaunderstine@gmail.com>
2025-12-02 14:09:24 -0500

    fix: refactor

--- Assets/Scripts/KinematicCharacterController.cs
+++ Assets/Scripts/KinematicCharacterController.cs
@@ -33,17 +33,9 @@ public class KinematicCharacterController : MonoBehaviour
 
     void FixedUpdate()
     {
-        groundCheckHitInfo = GroundCheck();
-        if (groundCheckHitInfo.collider != null)
-        {
-            //Debug.Log("Ground detected");
-            //Debug.Log(groundCheckHitInfo.distance);
-        }
-
         velocityVector = (this.transform.forward * movementInputs.y + this.transform.right * movementInputs.x).normalized * moveSpeed;
-        Vector3 newPosition = CalculateNewPosition(rb.position, collider, velocityVector);
+        Vector3 newPosition = CalculateNewPosition(rb.position, velocityVector, collider);
         rb.MovePosition(newPosition);
-        //Debug.Log(rb.position);
     }
 
     // TODO: refactor
@@ -54,24 +46,35 @@ public class KinematicCharacterController : MonoBehaviour
         return hitInfo;
     }
 
-    private Vector3 CalculateNewPosition(Vector3 start, CapsuleCollider collider, Vector3 velocityVector)
+    private Vector3 CalculateNewPosition(Vector3 start, Vector3 velocityVector, CapsuleCollider playerCollider)
     {
-        Vector3 moveVector = velocityVector * Time.fixedDeltaTime;
-        Vector3 newMoveVector = ProjectMovement(start, collider, moveVector);
+        Vector3 moveVector = GetMovementThisFrame(velocityVector);
+        Vector3 newMoveVector = ProjectMovement(start, playerCollider, moveVector);
         return start + newMoveVector;
+    }
+
+    private Vector3 GetMovementThisFrame(Vector3 velocityVector)
+    {
+        return velocityVector * Time.fixedDeltaTime;
     }
 
     private Vector3 ProjectMovement(Vector3 origin, CapsuleCollider collider, Vector3 moveVector)
     {
         Vector3 newMoveVector;
+        Vector3 collidePosition;
+        Vector3 remainingMove;
 
-        (Vector3 topPoint, Vector3 bottomPoint) spherePoints = CalculateSpheresInCapsule(origin, collider.height, collider.radius);
-        Debug.DrawLine(spherePoints.topPoint, spherePoints.bottomPoint, Color.red);
-        Debug.DrawLine(spherePoints.topPoint, spherePoints.topPoint + moveVector * 50f, Color.red);
-        if (Physics.CapsuleCast(spherePoints.topPoint, spherePoints.bottomPoint, collider.radius, moveVector.normalized, out RaycastHit moveProjectionHitInfo, moveVector.magnitude))
+        (Vector3 top, Vector3 bottom) sphereCenters = CalculateSpheresInCapsule(origin, collider.height, collider.radius);
+        Debug.DrawLine(sphereCenters.top, sphereCenters.bottom, Color.red);
+        Debug.DrawLine(sphereCenters.top, sphereCenters.top + moveVector * 50f, Color.red);
+        if (Physics.CapsuleCast(sphereCenters.top, sphereCenters.bottom, collider.radius, moveVector.normalized, out RaycastHit moveProjectionHitInfo, moveVector.magnitude))
         {
+            collidePosition = origin + moveVector.normalized * (moveProjectionHitInfo.distance - colliderPadding);
             newMoveVector = moveVector.normalized * (moveProjectionHitInfo.distance - colliderPadding); // Need to use colliderPadding or else collider will clip through wall
-        }
+            remainingMove = moveVector - newMoveVector;
+            //newMoveVector = Vector3.ProjectOnPlane(moveVector, moveProjectionHitInfo.normal);
+            newMoveVector = CollideAndSlide(collidePosition, remainingMove, moveProjectionHitInfo.normal);
+        } 
         else
         {
             newMoveVector = moveVector;
@@ -79,18 +82,23 @@ public class KinematicCharacterController : MonoBehaviour
 
         return newMoveVector;
     }
+
+    private Vector3 CollideAndSlide(Vector3 startPosition, Vector3 moveVector, Vector3 normal) // Add parameter to limit number of collideandslides
+    {
+        return Vector3.ProjectOnPlane(moveVector, normal);
+    }
     
     private (Vector3, Vector3) CalculateSpheresInCapsule(Vector3 capsuleCenter, float capsuleHeight, float capsuleRadius)
     {
         // TODO: this function needs to account for collider center relative to transform
-        Vector3 topPoint, bottomPoint;
+        Vector3 top, bottom;
         float yOffset;
 
         yOffset = capsuleHeight / 2f;
-        topPoint = capsuleCenter + new Vector3(0f, yOffset, 0f);
-        bottomPoint = capsuleCenter - new Vector3(0f, yOffset, 0f);
+        top = capsuleCenter + new Vector3(0f, yOffset, 0f);
+        bottom = capsuleCenter - new Vector3(0f, yOffset, 0f);
 
-        return (topPoint, bottomPoint);
+        return (top, bottom);
     }
 
     private void OnDrawGizmos()
@@ -98,11 +106,11 @@ public class KinematicCharacterController : MonoBehaviour
         if (Application.isPlaying)
         {
             Gizmos.DrawWireSphere(this.transform.position + (Vector3.down * (collider.height / 2f + groundCheckOffset)), collider.radius);
-            (Vector3 topPoint, Vector3 bottomPoint) spherePoints = CalculateSpheresInCapsule(this.transform.position, collider.height, collider.radius);
+            (Vector3 top, Vector3 bottom) sphereCenters = CalculateSpheresInCapsule(this.transform.position, collider.height, collider.radius);
             Vector3 moveVector = velocityVector * Time.fixedDeltaTime;
             Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(spherePoints.topPoint + moveVector, collider.radius);
-            Gizmos.DrawWireSphere(spherePoints.bottomPoint + moveVector, collider.radius);
+            Gizmos.DrawWireSphere(sphereCenters.top + moveVector, collider.radius);
+            Gizmos.DrawWireSphere(sphereCenters.bottom + moveVector, collider.radius);
         }
     }
 }
